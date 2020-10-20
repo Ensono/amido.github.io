500abfaa020a6da76901b701fe74d545b043cd42
corradot12 <corrado.turluianu@amido.com>
2020-10-20 18:10:30 +0100

    Feature/netcore content (#86)
    * Changes to content and styling
    
    * Re-warding
    
    * Added pipeline sidebar and page

--- sidebars.js
+++ sidebars.js
@@ -129,6 +129,13 @@ module.exports = {
                       },
                     ]
                   },
+                  {
+                    type: 'category',
+                    label: 'Infrastructure',
+                    items: [
+                      'workloads/azure/backend/netcore/infrastructure/pipeline_netcore',
+                    ]
+                  },
                   'workloads/azure/backend/netcore/logging_netcore',
                   'workloads/azure/backend/netcore/security_netcore',
                   {
