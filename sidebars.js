f12c61d4f5094c8d158a0cb5f980c3069d21d95a
Akrish17 <67281032+Akrish17@users.noreply.github.com>
2020-10-26 16:52:34 +0000

    Adding extra content for Java

--- sidebars.js
+++ sidebars.js
@@ -61,6 +61,7 @@ module.exports = {
                   'workloads/azure/backend/java/quickstart_java',
                   'workloads/azure/backend/java/scaffolding_java',
                   'workloads/azure/backend/java/ide_java',
+                  'workloads/azure/backend/java/setting_up_cosmos_db_locally',
                   {
                     type: "category",
                     label: "Architecture",
@@ -82,6 +83,7 @@ module.exports = {
                   'workloads/azure/backend/java_cqrs/quickstart_java_cqrs',
                   'workloads/azure/backend/java_cqrs/scaffolding_java_cqrs',
                   'workloads/azure/backend/java_cqrs/ide_java_cqrs',
+                  'workloads/azure/backend/java_cqrs/setting_up_cosmos_db_locally_cqrs',
                   {
                     type: "category",
                     label: "Architecture",
