9fc831e82ac45b8ef1bb11d2e881b4adb18e4005
Corrado Turluianu <corrado.turluianu@amido.com>
2020-11-11 10:56:39 +0000

    Bug fix for images

--- src/pages/Logos/RestAssuredLogo.tsx
+++ src/pages/Logos/RestAssuredLogo.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import useBaseUrl from '@docusaurus/useBaseUrl';
 
 function RestAssuredLogo(props) {
   return (
-    <img src="../../../static/img/rest-assured.svg" alt="Rest Assured" {...props}/>
+    <img src={useBaseUrl('img/rest-assured.svg')} alt="Rest Assured" {...props}/>
   );
 }
 
