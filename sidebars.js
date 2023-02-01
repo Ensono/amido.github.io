b1302d337ba19714ac1241d46f465cd3e037e793
Daniel Phillips <daniel.phillips@ensono.com>
2023-02-01 16:18:38 +0000

    Updates to current documentation

--- sidebars.js
+++ sidebars.js
@@ -281,9 +281,8 @@ module.exports = {
         type: "category",
         label: "Stacks Plugins",
         items: [
-          "nx/plugin-name/ensono-stacks-[plugin-name]",
-          "nx/azure-node/ensono-stacks-azure-node",
           "nx/workspace/ensono-stacks-workspace",
+          "nx/azure-node/ensono-stacks-azure-node",
         ]
       },
     ],
