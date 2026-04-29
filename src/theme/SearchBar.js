a8f58c5b8d0b640c83fa2602752bace9634998a0
Richard Slater <richard.slater@ensono.com>
2026-04-29 11:49:37 +0100

    chore(docusaurus): upgrade to 3.10.0
    Upgrade direct Docusaurus packages to 3.10.0 and refresh safe minor dependency updates.
    
    Also harden the custom search bar when Algolia credentials are unset and replace brittle legacy MDX HTML blocks that caused React hydration warnings on the examples page.

--- src/theme/SearchBar.js
+++ src/theme/SearchBar.js
@@ -1,20 +1,23 @@
-import React from 'react';
-
 import { DocSearch } from '@docsearch/react';
 import '@docsearch/css';
-import styles from './SearchBar.module.css'
+import './SearchBar.module.css';
 
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 
 export default function SearchBarWrapper() {
   const {
-    siteConfig: {customFields: {docSearch: {appId, apiKey, indexName}}}
-} = useDocusaurusContext()
+    siteConfig: {customFields: {docSearch: {appId, apiKey, indexName} = {}} = {}}
+  } = useDocusaurusContext();
+
+  if (!appId || !apiKey || !indexName) {
+    return null;
+  }
+
   return (
-      <DocSearch
-        appId={appId}
-        indexName={indexName}
-        apiKey={apiKey}
-      />
+    <DocSearch
+      appId={appId}
+      indexName={indexName}
+      apiKey={apiKey}
+    />
   );
 }
